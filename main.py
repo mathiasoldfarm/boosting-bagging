b4885f2cd6fb842418802e3b55758c540192f99d
webudvikleren <Hejmeddig195>
2021-03-07 14:17:15 +0100

    Data fetched

--- main.py
+++ main.py
@@ -7,63 +7,55 @@ import pandas as pd
 from log import log, reset_log
 
 reset_log()
-# base.fit(X_train, y_train)
-# base_train_score = base.score(X_train, y_train)
-# base_validation_score = base.score(X_validation, y_validation)
-# base_test_score = base.score(X_test, y_test)
+base.fit(X_train, y_train)
+base_train_score = base.score(X_train, y_train)
+base_test_score = base.score(X_test, y_test)
 
-# log(f"Base train score: {base_train_score}")
-# log(f"Base validation score: {base_validation_score}")
-# log(f"Base test score: {base_test_score}")
-# log("")
+log(f"Base train score: {base_train_score}")
+log(f"Base test score: {base_test_score}")
+log("")
 
-#bagging = BaggingCounting(base=base_type, n_bags=200, max_depth=1, max_leaf_nodes=2)
-#bagging.fit(X_train, y_train)
-# bagging_train_score = bagging.score(X_train, y_train)
-# bagging_validation_score = bagging.score(X_validation, y_validation)
-# bagging_test_score = bagging.score(X_test, y_test)
+bagging = BaggingCounting(base=base_type, n_bags=200, max_depth=1, max_leaf_nodes=2)
+bagging.fit(X_train, y_train)
+bagging_train_score = bagging.score(X_train, y_train)
+bagging_test_score = bagging.score(X_test, y_test)
 
-# log(f"Bagging train score: {bagging_train_score}")
-# log(f"Bagging validation score: {bagging_validation_score}")
-# log(f"Bagging test score: {bagging_test_score}")
-# log("")
+log(f"Bagging train score: {bagging_train_score}")
+log(f"Bagging test score: {bagging_test_score}")
+log("")
 
-# params = {
-#   "base": [base_type],
-#   "n_bags": [100, 200, 500, 1000],
-#   "max_depth": [4, 12, 24, 64],
-#   "max_leaf_nodes": [4, 24, 64, 128]
-# }
+params = {
+  "base": [base_type],
+  "n_bags": [100, 200, 500, 1000],
+  "max_depth": [4, 12, 24, 64],
+  "max_leaf_nodes": [4, 24, 64, 128]
+}
 
-# paramsearch = ParamSearch(BaggingCounting, params)
-# paramsearch.search(X_train, y_train, 2)
-# paramsearch.find_best()
+paramsearch = ParamSearch(BaggingCounting, params)
+paramsearch.search(X_train, y_train, 20)
+paramsearch.find_best()
 
-# paramsearch.fit(X_train, y_train)
+paramsearch.fit(X_train, y_train)
 
-# param_search_test_score = paramsearch.score(X_test, y_test)
-# log(f"Bagging with best params, test score: {param_search_test_score}")
+param_search_test_score = paramsearch.score(X_test, y_test)
+log(f"Bagging with best params, test score: {param_search_test_score}")
 
-# stump = base_type(**{"max_depth": 1, "max_leaf_nodes": 2})
-# stump.fit(X_train, y_train)
-# stump_train_score = stump.score(X_train, y_train)
-# stump_validation_score = stump.score(X_validation, y_validation)
-# stump_test_score = stump.score(X_test, y_test)
+stump = base_type(**{"max_depth": 1, "max_leaf_nodes": 2})
+stump.fit(X_train, y_train)
+stump_train_score = stump.score(X_train, y_train)
+stump_test_score = stump.score(X_test, y_test)
 
-# log(f"STUMP train score: {stump_train_score}")
-# log(f"STUMP validation score: {stump_validation_score}")
-# log(f"STUMP test score: {stump_test_score}")
-# log("")
+log(f"STUMP train score: {stump_train_score}")
+log(f"STUMP test score: {stump_test_score}")
+log("")
 
-# adaboost = AdaboostM1(base=base_type, iterations=200, max_depth=1, max_leaf_nodes=2)
-#adaboost.fit(X_train, y_train)
-# adaboost_train_score = adaboost.score(X_train, y_train)
-# adaboost_validation_score = adaboost.score(X_validation, y_validation)
-# adaboost_test_score = adaboost.score(X_test, y_test)
+adaboost = AdaboostM1(base=base_type, iterations=200, max_depth=1, max_leaf_nodes=2)
+adaboost.fit(X_train, y_train)
+adaboost_train_score = adaboost.score(X_train, y_train)
+adaboost_test_score = adaboost.score(X_test, y_test)
 
-# log(f"Adaboost train score: {adaboost_train_score}")
-# log(f"Adaboost validation score: {adaboost_validation_score}")
-# log(f"Adaboost test score: {adaboost_test_score}")
+log(f"Adaboost train score: {adaboost_train_score}")
+log(f"Adaboost test score: {adaboost_test_score}")
 
 params = {
   "base": [base_type],
